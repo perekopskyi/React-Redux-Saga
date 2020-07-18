229b7ab92b1a7ca0e1117665e4c42582b807e55d
i_am_yevhen <yevheniiperekopskyi@gmail.com>
2020-07-18 16:08:04 +0300

    Updates

---
+++ precache-manifest.e107f050bbf05dafd7b6e5566f3571b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f370c3259bcdf9639a86e94372cd7826",
+    "url": "/React-Redux-Saga/index.html"
+  },
+  {
+    "revision": "f7726064aad883584d77",
+    "url": "/React-Redux-Saga/static/js/2.d35504b8.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/React-Redux-Saga/static/js/2.d35504b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19760999be5e5502cef4",
+    "url": "/React-Redux-Saga/static/js/main.aac76584.chunk.js"
+  },
+  {
+    "revision": "a825ff6177af82f76060",
+    "url": "/React-Redux-Saga/static/js/runtime-main.0da17d29.js"
+  }
+]);
